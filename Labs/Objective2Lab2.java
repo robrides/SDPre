66ee577cb2002eee39d7f5fa59dc12a5d00a8baf
robrides <techjunkey@gmail.com>
2019-02-26 15:46:39 -0600

    Completed Objective 2 Lab 2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+    public static void main(String[] args) {
+      String name = "Rob Lounsbury";
+      int age = 46;
+      String state = "Maryland";
+      System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+    }
+}
