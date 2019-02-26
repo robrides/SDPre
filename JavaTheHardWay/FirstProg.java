8b8966905f8baee4b331a8c330239a1a655b0faa
robrides <techjunkey@gmail.com>
2019-02-25 18:25:29 -0600

    Completed jthw Exercise 1

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,10 @@
+public class FirstProg {
+
+  public static void main(String[] args) {
+
+    System.out.println("I, Rob Lounsbury, am determined to learn how to code.");
+    System.out.println("Today's date is Feburary 25, 2019.");
+
+  }
+
+}
