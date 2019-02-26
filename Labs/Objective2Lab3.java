5040a4d1c3f55b4f2a505148797e8cee4b5f2d32
robrides <techjunkey@gmail.com>
2019-02-26 15:51:52 -0600

    Completed Objective 2 Lab 3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,12 @@
+public class Objective2Lab3 {
+    public static void main(String[] args) {
+        String food1, food2, food3;
+
+        food1 = "Cookies";
+        food2 = "Sushi";
+        food3 = "Thai beef salad";
+
+        System.out.println("My Favorite Foods Are: \n" + "1. " + food1
+        + "\n2. " + food2 + "\n3. " + food3);
+    }
+}
