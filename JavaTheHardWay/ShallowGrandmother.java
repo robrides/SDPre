444b9883c1245ce147ad59e24e1120e8a7490be4
robrides <techjunkey@gmail.com>
2019-03-03 09:57:33 -0700

    Completed jthw ex 13

--- JavaTheHardWay/ShallowGrandmother.java
+++ JavaTheHardWay/ShallowGrandmother.java
@@ -4,8 +4,8 @@ public class ShallowGrandmother {
     public static void main(String[] args) {
       Scanner keyboard = new Scanner(System.in);
       int age;
-      double income, cute;
-      boolean allowed;
+      double income, cute, happy;
+      boolean allowed, allowed2;
 
       System.out.print("Enter your age: ");
       age = keyboard.nextInt();
@@ -16,9 +16,13 @@ public class ShallowGrandmother {
       System.out.print("How cute are you, on a scale from 0.0 to 10.0? ");
       cute = keyboard.nextDouble();
 
-      allowed = (age > 25 && age < 40 && (income > 50000 || cute >= 8.5));
+      System.out.print("How happy do you make them? ");
+      happy = keyboard.nextDouble();
 
-      System.out.println("Allowed to date my grandchild? " + allowed);
+      // allowed = (age > 25 && age < 40 && (income > 50000 || cute >= 8.5));
+      allowed2 = (age > 42 && age < 48 && (happy > 7.0));
+
+      System.out.println("Allowed to date my grandchild? " + allowed2);
 
     }
 }
