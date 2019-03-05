115819b9a41d7458365ff6b5d2b2ec539c03f4ea
robrides <techjunkey@gmail.com>
2019-03-04 18:31:04 -0700

    Completed obj 6 lab 4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,11 @@
+public class Objective6Lab4 {
+  public static void main(String[] args) {
+    int count = 1, sumTotal = 0;
+
+    while (count <= 20) {
+      sumTotal = sumTotal + count;
+      count++;
+    }
+    System.out.print(sumTotal);
+  }
+}
